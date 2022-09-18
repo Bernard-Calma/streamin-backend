ba99c55ecdad1daef8b5cf393c015ba010e6de19
nard.calma@gmail.com <nard.calma@gmail.com>
2022-09-18 02:13:04 -0700

    video model/schema crated

---
+++ models/videos.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema({
+    name: { 
+        type: String,
+        required: true,
+    },
+    user: { // will need to be attached to logged in user
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: String,
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    comments: [{
+        body: String,
+        commentDate: Date
+    }],
+    publishedDate: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+const Video = mongoose.model("Video", videoSchema)
+
+module.exports = Video;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-//Express
+// Express
 const express = require("express");
 const app = express();
 
+// Port
 const PORT = process.env.PORT || 3000;
 
-//DB Connection
+// DB Connection
 require("./config/db.connection")
 
+// Listen
 app.listen(PORT, () => {
     console.log("Server is running at PORT: " + PORT)
 })
