291819a20cdcf7e1e3615fd6be7c70b24e2ee4df
nard.calma@gmail.com <nard.calma@gmail.com>
2022-09-18 01:33:18 -0700

    installed express

---
+++ config/db.connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const mongodbURL = process.env.MONGODB_URI || "mongodb://localhost:27017/streaminDB";
+
+// set up connection with the DB
+mongoose.connect(mongodbURL);
+
+// set up listeners to monitor your database connection
+mongoose.connection.on('connected', ()=> console.log('DB connected... 🙌🙌🙌'));
+mongoose.connection.on('error', (err)=> console.log(err.message));
+mongoose.connection.on('disconnected', ()=> console.log('mongoose disconnected'));
