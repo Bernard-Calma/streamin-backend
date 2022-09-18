f6c9f5904619634c437bc966b72a0705662c1b5e
nard.calma@gmail.com <nard.calma@gmail.com>
2022-09-18 01:35:38 -0700

    setup server connection

---
+++ server.js
@@ -0,0 +1,9 @@
+//Express
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server is running at PORT: " + PORT)
+})
