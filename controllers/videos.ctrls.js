9a9a1801e0b30d444f5262638bf1c455c19a0540
nard.calma@gmail.com <nard.calma@gmail.com>
2022-09-18 02:29:53 -0700

    /video get route fixed tested and working

--- controllers/videos.ctrls.js
+++ controllers/videos.ctrls.js
@@ -2,7 +2,7 @@ const db = require("../models")
 
 // ROUTES
 const index = (req, res) => {
-    db.Videos.find({}, (err, video) => {
+    db.videos.find({}, (err, video) => {
         if(err) return res.status(400).json({error: err.message})
         return res.status(200).json({
             video,
--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const PORT = process.env.PORT || 3000;
 // DB Connection
 require("./config/db.connection")
 
+// Internal Modules
+const routes = require("./routes")
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Routes
+app.use("/videos", routes.videos);
+app.get("/", (req, res) => {
+    res.send("Hello page"); 
+})
+
 // Listen
 app.listen(PORT, () => {
     console.log("Server is running at PORT: " + PORT)
