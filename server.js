971edcbc08afad30d2242b0279228b2ad503a3a1
nard.calma@gmail.com <nard.calma@gmail.com>
2022-09-18 01:37:27 -0700

    connected to DB successfull

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//DB Connection
+require("./config/db.connection")
+
 app.listen(PORT, () => {
     console.log("Server is running at PORT: " + PORT)
 })
