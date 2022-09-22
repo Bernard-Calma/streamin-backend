e6e9b06aa950f9af782e6cfe18500a0dd0f58c66
nard.calma@gmail.com <nard.calma@gmail.com>
2022-09-21 17:44:15 -0700

    brcypt installed and added

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ctrls = require("../controllers")
 
+router.get("/", ctrls.users.index)
 router.post("/", ctrls.users.create)
 router.get("/:id", ctrls.users.show)
 router.put("/:id",ctrls.users.edit)
