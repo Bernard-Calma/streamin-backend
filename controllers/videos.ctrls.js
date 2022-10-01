15ca1ddcb76cee88e9d47410fb0c1794571333a2
nard.calma@gmail.com <nard.calma@gmail.com>
2022-10-01 11:33:54 -0700

    added conditions to websites user wants to pull videos from

--- controllers/videos.ctrls.js
+++ controllers/videos.ctrls.js
@@ -33,6 +33,42 @@ const userVideos = (req, res) => {
 // CREATE
 // post - /videos
 const create = (req, res) => {
+    // console.log(req.body.videoLink)
+    let postfix = ""
+    if(req.body.videoLink){
+        // YouTube
+        if(req.body.videoLink.includes("youtube")){
+            // console.log(req.body.videoLink.lastIndexOf('/'))
+            if(req.body.videoLink.lastIndexOf('/') >= 25){
+                postfix = req.body.videoLink.slice(req.body.videoLink.lastIndexOf('/') + 1);
+            } else {
+                // console.log(req.body.videoLink.lastIndexOf('='))
+                if (req.body.videoLink.lastIndexOf('=') >= 45){
+                    postfix = req.body.videoLink.slice(req.body.videoLink.indexOf('=') + 1,req.body.videoLink.lastIndexOf('=') - 2)  
+                    // console.log(postfix)
+                } else {
+                    postfix = req.body.videoLink.slice(req.body.videoLink.lastIndexOf('=') + 1);
+                }
+            } 
+            // console.log(postfix)
+            req.body.videoLink = `https://www.youtube.com/embed/${postfix}`
+        } else if (req.body.videoLink.includes("vimeo")){
+            postfix = req.body.videoLink.slice(req.body.videoLink.lastIndexOf('/') + 1)
+            req.body.videoLink = `https://player.vimeo.com/video/${postfix}`
+        } else if (req.body.videoLink.includes("facebook")){
+            let user;
+            // console.log(req.body.videoLink.slice(req.body.videoLink.indexOf('.com/') + 5, req.body.videoLink.indexOf('/', req.body.videoLink.indexOf('.com/') + 6)))
+            user = req.body.videoLink.slice(req.body.videoLink.indexOf('.com/') + 5, req.body.videoLink.indexOf('/', req.body.videoLink.indexOf('.com/') + 6))
+            // console.log(req.body.videoLink.slice(req.body.videoLink.lastIndexOf('/') + 1))
+            postfix = req.body.videoLink.slice(req.body.videoLink.lastIndexOf('/') + 1)
+            req.body.videoLink = `https://www.facebook.com/plugins/video.php?height=401&href=https%3A%2F%2Fwww.facebook.com%2F${user}%2Fvideos%2F${postfix}%2F&show_text=false&width=560&t=0`
+        } else if (req.body.videoLink.includes("dailymotion")){
+            postfix = req.body.videoLink.slice(req.body.videoLink.lastIndexOf('/') + 1, req.body.videoLink.lastIndexOf('/') + 8);
+            // console.log(postfix)
+            req.body.videoLink = `https://www.dailymotion.com/embed/video/${postfix}`
+            // console.log(req.body.videoLink)
+        }
+    }    
     db.videos.create(req.body, (err, video) => {
         if(err) return res.status(404).json({error: err.message})
         return res.status(200).json(video)
